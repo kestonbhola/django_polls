53b9d43ea06dd19e7db28bbaed9acee1a864cc5a
kestonbhola <kbhola001@sgu.edu>
2025-04-21 18:42:48 -0400

    Commit at the end of part 3

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,6 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request,"polls/index.html", context)
 
-
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
