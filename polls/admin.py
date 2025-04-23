7991b7e401127fa0c5eb77fee5b88a5bb574d03e
kestonbhola <kbhola001@sgu.edu>
2025-04-22 22:42:43 -0400

    Updated admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -5,5 +5,5 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ["pub_date", "question_text"]
-    
-admin.site.register(Question)
+
+admin.site.register(Question, QuestionAdmin)
