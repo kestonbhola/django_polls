bf7fb0e39558503bf4be7eb2436b589f377aef63
kestonbhola <kbhola001@sgu.edu>
2025-05-01 08:59:51 -0400

    Updated admin page

--- polls/admin.py
+++ polls/admin.py
@@ -5,14 +5,19 @@ from .models import Choice,Question
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 2 # Number of extra blank choice lines to display
 
 class QuestionAdmin(admin.ModelAdmin):
+    # The fields to be used in displaying a particular item the Question model for editing
     fieldsets = [
         ("Identifying Information", {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
+
+    # The fields to be used when listing the data in the Question model
     list_display = ("question_text", "pub_date", "was_published_recently")
+    
+    list_filter = ["pub_date"]
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin #important for admin interface styling
 
 
 class Question(models.Model):
@@ -12,6 +13,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    #@admin.display() decorator is used to customize the display of the method in the admin interface.
+    #boolean=True tells the admin to display the result as a checkmark (✔/✖) instead of True/False.
+    #ordering='pub_date' allows the admin list view to sort rows based on the pub_date 
+    #   field whensorting by was_published_recently.
+    #It also adds a nice label (defaults to the method name, 
+    #   but you can override it with description="..." if desired).
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently?")
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
