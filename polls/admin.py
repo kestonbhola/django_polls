33776c1340ccc5fb69ff3c9844d0125e9ce9fbdc
kestonbhola <kbhola001@sgu.edu>
2025-04-30 21:59:18 -0400

    Modifying admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice,Question
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Identifying Information", {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
